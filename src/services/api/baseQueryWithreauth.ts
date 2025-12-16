aa831bed670fc297c6b51db9974ba64c8765fd9c
Rawan Mohamed <rawan.mohamed@iken.tech>
2025-12-16 15:03:31 +0000

    Merged PR 6534: feature/handle refresh token logic with baseQueryWithReauth
    **1- Custom Base Query**
    
    Automatically injects the Authorization header with the current access token from Redux.
    
    **2- Automatic Token Refresh**
    
    Detects 401 Unauthorized responses from the API.
    
    Uses the refresh token from Redux to request a new access token.
    
    Updates the Redux store with the new credentials using setCredentials.
    
    Retries the original API request transparently after successful refresh.
    
    **3- Error Handling**
    
    If the refresh token is missing or the refresh attempt fails:
    
    Displays a user-friendly toast notification: "Session expired. Please login again."
    
    Related work items: #9111

--- src/services/api/baseQueryWithreauth.ts
+++ src/services/api/baseQueryWithreauth.ts
@@ -1,17 +1,20 @@
 import type { RootState } from '@/app/store';
+import { setCredentials } from '@/features/auth/authSlice';
+import type { CredentialsType } from '@/types/auth.types';
 import {
   fetchBaseQuery,
   type BaseQueryFn,
   type FetchArgs,
   type FetchBaseQueryError,
 } from '@reduxjs/toolkit/query';
+import { toast } from 'react-toastify';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: import.meta.env.VITE_BASE_URL,
   prepareHeaders: (headers, { getState }) => {
     const accessToken = (getState() as RootState).auth.accessToken;
     if (accessToken) {
-      headers.set('authorization', accessToken);
+      headers.set('authorization', `Bearer ${accessToken}`);
     }
     return headers;
   },
@@ -24,5 +27,35 @@ export const baseQueryWithReauth: BaseQueryFn<
 > = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions);
   console.log(result);
+
+  if (result?.meta?.response?.status === 401) {
+    const refreshToken = (api.getState() as RootState).auth.refreshToken;
+    const id = (api.getState() as RootState).auth.user?.id;
+    if (!refreshToken || !id) {
+      toast.error('Session expired. Please login again.');
+      // logout redux state will be added later
+      return result;
+    }
+    const refreshResult = await baseQuery(
+      {
+        url: 'auth/refreshToken',
+        method: 'POST',
+        body: {
+          userId: id,
+          token: refreshToken,
+        },
+      },
+      api,
+      extraOptions
+    );
+    if (refreshResult?.data) {
+      const returnedResult = refreshResult?.data as CredentialsType;
+      api.dispatch(setCredentials(returnedResult));
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      toast.error('Session expired. Please login again.');
+      // logout redux state will be added later
+    }
+  }
   return result;
 };
