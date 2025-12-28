860895fee8907c112f8256035fd35fd104e519c5
Rawan Mohamed <rawan.mohamed@iken.tech>
2025-12-28 15:01:19 +0000

    Merged PR 6664: Delete Restaurant
    Introduced a DeleteRestaurantDialog modal triggered by the delete action.
    
    The modal clearly asks the user to confirm the deletion action.
    
    Added Yes / No actions:
    
    Yes: Executes the delete request and removes the restaurant.
    
    No: Cancels the action and closes the modal.
    
    Implemented keyboard-accessible button navigation for better usability and accessibility.
    
    Added success and error toast notifications to provide immediate user feedback.
    
    Improved modal UI with rounded corners and responsive width for better visual consistency.
    
    ![image (2).png](https://dev.azure.com/ikentechnology/c8ab89bb-3867-421d-9adc-13cea45e64c7/_apis/git/repositories/a4357018-059a-4270-afaf-53d791ca4d9b/pullRequests/6664/attachments/image%20%282%29.png)
    ![image (3).png](https://dev.azure.com/ikentechnology/c8ab89bb-3867-421d-9adc-13cea45e64c7/_apis/git/repositories/a4357018-059a-4270-afaf-53d791ca4d9b/pullRequests/6664/attachments/image%20%283%29.png)
    
    Related work items: #9331

---
+++ src/components/Shared/portals/DeleteRestaurantDialog.tsx
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Tooltip from '@mui/material/Tooltip';
+import Button from '@mui/material/Button';
+import { MdDelete } from 'react-icons/md';
+import { toast } from 'react-toastify';
+import { useDeleteRestaurantMutation } from '@/features/restuarants/restaurantsApi';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  py: 2.5,
+  px: 3,
+  borderRadius: 4,
+  width: { xs: '80%', sm: 400, md: 450 },
+};
+
+export default function DeleteRestaurantDialog({ id }: { id: string }) {
+  const [deleteRestaurant] = useDeleteRestaurantMutation();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleDelete = async () => {
+    try {
+      await deleteRestaurant({ id }).unwrap();
+      toast.success('Restaurant Deleted Successfully.');
+      handleClose();
+    } catch (err) {
+      toast.error('Something Went Wrong. Please Try again.');
+    }
+  };
+
+  return (
+    <div>
+      <Tooltip title='Delete'>
+        <button
+          className='action-btn text-[#ee2f2f] hover:bg-[#fee2e2]'
+          onClick={handleOpen}
+        >
+          <MdDelete />
+        </button>
+      </Tooltip>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+      >
+        <Box sx={style}>
+          <h2 className='text-gray-800 font-medium font-sans text-xl '>
+            Confirm Action
+          </h2>
+          <p className='mt-4 text-gray-500'>
+            Are you sure you want to Delete this Restaurant?
+          </p>
+          <div className='flex justify-between items-center mt-5'>
+            <Button
+              autoFocus
+              tabIndex={0}
+              sx={{
+                color: '#ee2f2f',
+                '&:hover': {
+                  background: '#fee2e2',
+                  borderRadius: '20px',
+                },
+              }}
+              onClick={handleDelete}
+            >
+              Yes
+            </Button>
+            <Button
+              sx={{
+                '&:hover': {
+                  borderRadius: '20px',
+                },
+              }}
+              onClick={handleClose}
+              tabIndex={0}
+            >
+              No
+            </Button>
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
